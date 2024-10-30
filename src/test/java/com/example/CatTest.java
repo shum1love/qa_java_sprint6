4e10cb55c4941bcc7725da7a41db460d70e96712
shum1love <rodya7911@gmail.com>
2024-10-30 11:00:46 +0300

    add changes in class CatTest

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,17 +2,14 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.util.List;  // Импортируем List для проверки возвращаемых значений
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-public class CatTest {
+import java.util.List;
 
-    private Cat cat; // Экземпляр класса Cat
-    private Feline felineMock; // Мок для доступа к классу Feline
+public class CatTest {
+    private Cat cat;
+    private Feline felineMock;
 
     @Before
     public void setUp() {
@@ -22,21 +19,13 @@ public class CatTest {
 
     @Test
     public void testGetSound() {
-        String sound = cat.getSound(); // Получаем звук из cat
-        assertEquals("Мяу", sound); // Проверяем, соответствует ли он "Мяу"
+        assertEquals("Мяу", cat.getSound());
     }
 
     @Test
     public void testGetFood() throws Exception {
-        // Настраиваем, чтобы getFood возвращал желаемый результат от мокированного Feline
-        when(felineMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-
-        // Убедимся, что метод getFood() у кота возвращает правильные значения
+        when(felineMock.eatMeat()).thenReturn(List.of("Животные"));
         List<String> food = cat.getFood();
-
-        // Проверяем результат
         assertTrue(food.contains("Животные"));
-        assertTrue(food.contains("Птицы"));
-        assertTrue(food.contains("Рыба"));
     }
 }
